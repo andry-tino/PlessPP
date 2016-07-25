9387250ef02678cc2f34d9631a497523feac75b1
Andrea <andry.tino@gmail.com>
2016-07-25 10:43:29 +0200

    Missing stuff

--- spike/officeAddIn/slideChangerPowerPointOfficeAddIn/app/home/home.js
+++ spike/officeAddIn/slideChangerPowerPointOfficeAddIn/app/home/home.js
@@ -5,24 +5,40 @@
   Office.initialize = function(reason){
     jQuery(document).ready(function(){
       app.initialize();
-      jQuery('#get-data-from-selection').click(getDataFromSelection);
+
+      jQuery('#get-curr-slide-index').click(getCurrentSlideIndex);
+      jQuery('#goto-next-slide').click(gotoNextSlide);
     });
   };
 
-  // Reads data from current document selection and displays a notification
-  function getDataFromSelection(){
+  // Gets the current slide index
+  function getCurrentSlideIndex() {
     if (Office.context.document.getSelectedDataAsync) {
-      Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-        function(result){
-          if (result.status === Office.AsyncResultStatus.Succeeded) {
-            app.showNotification('The selected text is:', '"' + result.value + '"');
-          } else {
-            app.showNotification('Error:', result.error.message);
-          }
+      Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function(result) {
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+          app.showNotification('Current slide is:', '"' + result.value.slides[0] + '"');
+        } else {
+          app.showNotification('Error:', result.error.message);
         }
-        );
+      });
     } else {
-      app.showNotification('Error:', 'Reading selection data not supported by host application.');
+      app.showNotification('Error:', 'Slide index retreival is not supported!');
     }
+  }
+
+  // Moves to the next slide
+  function gotoNextSlide() {
+    var frst = Office.Index.First;
+    var last = Office.Index.Last;
+    var next = Office.Index.Next;
+    var prev = Office.Index.Previous;
+
+    Office.context.document.goToByIdAsync(Next, Office.GoToType.Index, function(result) {
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+            app.showNotification("Next slide successful");
+        } else {
+            app.showNotification("Error: " + result.error.message);
+        }
+    });
   }
 })();
