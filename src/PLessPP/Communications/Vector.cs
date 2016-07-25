d6cc0f869cb1c1e60b81981da9b3a9b75c7bab27
Andrea <andry.tino@gmail.com>
2016-07-25 17:22:43 +0200

    More stuff

---
+++ src/PLessPP/Communications/Vector.cs
@@ -0,0 +1,34 @@
+﻿/// <summary>
+/// Vector.cs - 2015
+/// </summary>
+
+namespace PLessPP.Communications
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class Vector
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember(Name = "AccX", IsRequired = true)]
+        public double AccelerationX { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember(Name = "AccY", IsRequired = true)]
+        public double AccelerationY { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember(Name = "AccZ", IsRequired = true)]
+        public double AccelerationZ { get; set; }
+    }
+}
