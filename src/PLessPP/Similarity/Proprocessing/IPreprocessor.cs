eec08ef77f58c103ba58f8d27d06186bea233031
Andrea <andry.tino@gmail.com>
2016-07-25 16:34:56 +0200

    Introducing preprocessors

---
+++ src/PLessPP/Similarity/Proprocessing/IPreprocessor.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// IPreprocessor.cs
+/// </summary>
+
+namespace PLessPP.Similarity
+{
+    using System;
+
+    using PLessPP.Similarity.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPreprocessor
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        Sequence Preprocess(Sequence sequence);
+    }
+}
