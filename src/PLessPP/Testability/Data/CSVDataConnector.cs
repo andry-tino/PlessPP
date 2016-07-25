72b927ae571c10f8585b228305b0df33f42f7146
Andrea <andry.tino@gmail.com>
2016-07-25 17:44:54 +0200

    Adding data connectors

---
+++ src/PLessPP/Testability/Data/CSVDataConnector.cs
@@ -0,0 +1,73 @@
+﻿/// <summary>
+/// CSVDataConnector.cs
+/// </summary>
+
+namespace PLessPP.Testing.Testability.Data
+{
+    using System;
+    using System.IO;
+
+    using PLessPP.Similarity.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class CSVDataConnector : IDataConnector
+    {
+        private Sequence sequence;
+        private string filePath;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public CSVDataConnector(string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException("Invalid file path.", nameof(path));
+            }
+
+            this.filePath = path;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Sequence Data
+        {
+            get
+            {
+                if (this.sequence == null)
+                {
+                    this.GetData();
+                }
+
+                return this.sequence;
+            }
+        }
+
+        private void GetData()
+        {
+            string content = File.ReadAllText(this.filePath);
+
+            // Split commans and get values
+            string[] numbers = content.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Convert to double
+            double[] values = new double[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                values[i] = double.Parse(numbers[i]);
+            }
+
+            this.sequence = new Sequence(values);
+        }
+    }
+}
---
+++ src/PLessPP/Testability/Data/IDataConnector.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// IDataConnector.cs
+/// </summary>
+
+namespace PLessPP.Testing.Testability.Data
+{
+    using System;
+
+    using PLessPP.Similarity.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDataConnector
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Sequence Data { get; }
+    }
+}
