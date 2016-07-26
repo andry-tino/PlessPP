8d54356161d7788d1ede52f3738d4c54de009d38
Andrea <andry.tino@gmail.com>
2016-07-26 14:35:29 +0200

    Introducing chunk buffer interface

---
+++ src/PLessPP/AI/IChunkBuffer.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// IChunkBuffer.cs
+/// </summary>
+
+namespace PLessPP.AI
+{
+    using System;
+
+    using PLessPP.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IChunkBuffer
+    {
+        Point[] GetCurrentChunk();
+    }
+}
---
+++ src/PLessPP/AI/SequenceSearch/MultiWindowMultiShiftSearch.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// MultiWindowMultiShiftSearch.cs
+/// </summary>
+
+namespace PLessPP.AI
+{
+    using System;
+
+    using PLessPP.Data;
+    using Similarity.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MultiWindowMultiShiftSearch : ISequenceSearcher
+    {
+        private readonly int shift;
+        private readonly int[] windowSizes;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shift"></param>
+        /// <param name="windowSizes"></param>
+        public MultiWindowMultiShiftSearch(int shift, int[] windowSizes)
+        {
+            // TODO: Add checks
+
+            this.shift = shift;
+            this.windowSizes = windowSizes;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <param name="results"></param>
+        public void Search(Sequence sequence, out object results)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
