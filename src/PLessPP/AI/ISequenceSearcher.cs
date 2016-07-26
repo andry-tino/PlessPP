f4da78afcbbcd91438eea99b60ee225a42bd43b0
Andrea <andry.tino@gmail.com>
2016-07-26 13:00:02 +0200

    Improving tests + adding Euclidean distance

---
+++ src/PLessPP/AI/ISequenceSearcher.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// ISequenceSearcher.cs
+/// </summary>
+
+namespace PLessPP.AI
+{
+    using System;
+
+    using PLessPP.Data;
+    using PLessPP.Similarity.Data;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISequenceSearcher
+    {
+        /// <summary>
+        /// Searches a particular sequence inside another one.
+        /// </summary>
+        /// <param name="sequence">The <see cref="Sequence"/> to search into.</param>
+        /// <param name="result"></param>
+        void Search(Sequence sequence, out int results); // not int, we need to create a class for this
+    }
+}
--- src/PLessPP/UnitTests/Validity/DTWSequenceMatchingTestSuite.cs
+++ src/PLessPP/UnitTests/Validity/DTWSequenceMatchingTestSuite.cs
@@ -13,17 +13,19 @@ namespace PLessPP.Testing
     using PLessPP.Similarity;
     using PLessPP.Testing.Testability.Data;
     using PLessPP.Testing.Validity.TestObjects;
-    
+
     /// <summary>
     /// This test suite considers twi sequences of data from real sampling and compares them.
     /// 
     /// Legend:
-    /// Name is as follows: Scenario_<tester-type>_<hand>_<matching-config>
+    /// Name is as follows: Scenario_<tester-type>_<hand>_<matching-config>_<distance-algo>
     /// - tester-type:      D  = Developer
     /// - hand:             Sx = Left
     ///                     Rx = Right
     /// - matching-config:  P  = Positive
     ///                     N  = Negative
+    /// - distance-algo:    A  = Plain absolute difference
+    ///                     E  = Euclidean (not squared)
     /// </summary>
     [TestClass]
     public class DTWSequenceMatchingTestSuite
@@ -32,20 +34,33 @@ namespace PLessPP.Testing
         /// Two different developers.
         /// Two negative sequences.
         /// Left hand.
+        /// Absolute distance.
         /// </summary>
         [TestMethod]
-        public void Scenario_D1xD2_Sx_NxN()
+        public void Scenario_D1xD2_Sx_NxN_A()
         {
-            CSVDataConnector dataConnector1 = new CSVDataConnector(TestObjectsProvider.SampleDataNegativeDev1FilePath);
-            CSVDataConnector dataConnector2 = new CSVDataConnector(TestObjectsProvider.SampleDataNegativeDev2FilePath);
-
-            Sequence sequence1 = dataConnector1.Data;
-            Sequence sequence2 = dataConnector2.Data;
-
-            ISimilarityAlgorithm dtwAlgorithm = new DynamicTimeWarpingAlgorithm(
+            double similarity = GetDistance(
+                TestObjectsProvider.SampleDataNegativeDev1FilePath,
+                TestObjectsProvider.SampleDataNegativeDev2FilePath,
                 new AbsoluteDifferencePointDistanceCalculator());
 
-            double similarity = dtwAlgorithm.ComputeSimilarity(sequence1, sequence2);
+            Assert.AreNotEqual(0, similarity, "Two negative sequences from different testers should not give zero similarity!");
+        }
+
+        /// <summary>
+        /// Two different developers.
+        /// One negative sequence.
+        /// One positive sequence.
+        /// Left hand.
+        /// Absolute distance.
+        /// </summary>
+        [TestMethod]
+        public void Scenario_D1xD2_Sx_NxP_A()
+        {
+            double similarity = GetDistance(
+                TestObjectsProvider.SampleDataNegativeDev1FilePath,
+                TestObjectsProvider.SampleDataPositiveDev31FilePath,
+                new AbsoluteDifferencePointDistanceCalculator());
 
             Assert.AreNotEqual(0, similarity, "Two negative sequences from different testers should not give zero similarity!");
         }
@@ -54,20 +69,15 @@ namespace PLessPP.Testing
         /// One developer.
         /// Two positive sequences.
         /// Left hand.
+        /// Absolute distance.
         /// </summary>
         [TestMethod]
-        public void Scenario_D1_Sx_PxP()
+        public void Scenario_D1_Sx_PxP_A()
         {
-            CSVDataConnector dataConnector1 = new CSVDataConnector(TestObjectsProvider.SampleDataPositiveDev31FilePath);
-            CSVDataConnector dataConnector2 = new CSVDataConnector(TestObjectsProvider.SampleDataPositiveDev32FilePath);
-
-            Sequence sequence1 = dataConnector1.Data;
-            Sequence sequence2 = dataConnector2.Data;
-
-            ISimilarityAlgorithm dtwAlgorithm = new DynamicTimeWarpingAlgorithm(
+            double similarity = GetDistance(
+                TestObjectsProvider.SampleDataPositiveDev31FilePath,
+                TestObjectsProvider.SampleDataPositiveDev32FilePath,
                 new AbsoluteDifferencePointDistanceCalculator());
-
-            double similarity = dtwAlgorithm.ComputeSimilarity(sequence1, sequence2);
 
             Assert.AreNotEqual(0, similarity, "Two negative sequences from different testers should not give zero similarity!");
         }
@@ -76,22 +86,30 @@ namespace PLessPP.Testing
         /// Two different developers.
         /// Two positive sequences.
         /// Left hand.
+        /// Absolute distance.
         /// </summary>
         [TestMethod]
-        public void Scenario_D1xD2_Sx_PxP()
+        public void Scenario_D1xD2_Sx_PxP_A()
         {
-            CSVDataConnector dataConnector1 = new CSVDataConnector(TestObjectsProvider.SampleDataPositiveDev4FilePath);
-            CSVDataConnector dataConnector2 = new CSVDataConnector(TestObjectsProvider.SampleDataPositiveDev5FilePath);
+            double similarity = GetDistance(
+                TestObjectsProvider.SampleDataPositiveDev4FilePath, 
+                TestObjectsProvider.SampleDataPositiveDev5FilePath,
+                new AbsoluteDifferencePointDistanceCalculator());
+
+            Assert.AreNotEqual(0, similarity, "Two negative sequences from different testers should not give zero similarity!");
+        }
+
+        private static double GetDistance(string file1, string file2, IPointDistanceCalculator distanceCalculator)
+        {
+            CSVDataConnector dataConnector1 = new CSVDataConnector(file1);
+            CSVDataConnector dataConnector2 = new CSVDataConnector(file2);
 
             Sequence sequence1 = dataConnector1.Data;
             Sequence sequence2 = dataConnector2.Data;
 
-            ISimilarityAlgorithm dtwAlgorithm = new DynamicTimeWarpingAlgorithm(
-                new AbsoluteDifferencePointDistanceCalculator());
+            ISimilarityAlgorithm dtwAlgorithm = new DynamicTimeWarpingAlgorithm(distanceCalculator);
 
-            double similarity = dtwAlgorithm.ComputeSimilarity(sequence1, sequence2);
-
-            Assert.AreNotEqual(0, similarity, "Two negative sequences from different testers should not give zero similarity!");
+            return dtwAlgorithm.ComputeSimilarity(sequence1, sequence2);
         }
     }
 }
