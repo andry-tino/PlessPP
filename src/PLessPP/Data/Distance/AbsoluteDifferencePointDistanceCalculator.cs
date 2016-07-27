bb6412c2dd0b8937df36f925ddcae1d8ccf796f1
Liansheng Hua <hualiansheng@gmail.com>
2016-07-27 03:14:20 +0200

    adjust the absolutedistance. introduce a weight to calculate gyroscope data as well.

--- src/PLessPP/Data/Distance/AbsoluteDifferencePointDistanceCalculator.cs
+++ src/PLessPP/Data/Distance/AbsoluteDifferencePointDistanceCalculator.cs
@@ -8,6 +8,7 @@ namespace PLessPP.Data
 
     public class AbsoluteDifferencePointDistanceCalculator : IPointDistanceCalculator
     {
+        private const double alpha = 0.5;
         /// <summary>
         /// 
         /// </summary>
@@ -19,10 +20,14 @@ namespace PLessPP.Data
         /// </remarks>
         public double GetDistance(Point point1, Point point2)
         {
-            return
-                Math.Abs(point1.AccelerationX - point2.AccelerationX) +
-                Math.Abs(point1.AccelerationY - point2.AccelerationY) +
-                Math.Abs(point1.AccelerationZ - point2.AccelerationZ);
+            return alpha * 
+                (Math.Abs(point1.AccelerationX - point2.AccelerationX) + 
+                Math.Abs(point1.AccelerationY - point2.AccelerationY) + 
+                Math.Abs(point1.AccelerationZ - point2.AccelerationZ)) + 
+                (1.0 - alpha) * 
+                (Math.Abs(point1.GyroX - point2.GyroX) +
+                Math.Abs(point1.GyroY - point2.GyroY) +
+                Math.Abs(point1.GyroZ - point2.GyroZ));
         }
     }
 }
