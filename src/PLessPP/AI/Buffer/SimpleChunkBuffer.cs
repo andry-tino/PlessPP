35dbf8fe1e04fef87620233b079b01c17a74a8d4
samlieshout <samlieshout@hotmail.com>
2016-07-27 02:40:43 +0200

    Wire up to AI (needs tidy)

---
+++ src/PLessPP/AI/Buffer/SimpleChunkBuffer.cs
@@ -0,0 +1,57 @@
+﻿using PLessPP.Data;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLessPP.AI
+{
+    {
+
+        private const int SecondsOfData = 4;
+        private const int ItemsPerSecond = 64;
+        private const int ItemsInChunk = SecondsOfData * ItemsPerSecond;
+
+        private readonly ConcurrentQueue<Point> dataPoints = new ConcurrentQueue<Point>();
+
+        public SimpleChunkBuffer()
+        {
+        }
+
+        public Point[] GetCurrentChunk()
+        {
+            if (this.dataPoints.Count() < ItemsInChunk)
+            {
+                return null; // not enough data yet
+            }
+
+            lock (this.dataPoints)
+            {
+                return this.dataPoints.ToArray();
+            }
+        }
+
+        public void ClearBuffer()
+        {
+            lock (this.dataPoints)
+            {
+                Point toChug;
+                while (this.dataPoints.TryDequeue(out toChug)) ;
+            }
+        }
+
+        public void EnqueueData(Point datapoint)
+        {
+            this.dataPoints.Enqueue(datapoint);
+
+            // Throw out old data
+            if (this.dataPoints.Count > ItemsInChunk)
+            {
+                Point toChug;
+                this.dataPoints.TryDequeue(out toChug);
+            }
+        }
+    }
+}
