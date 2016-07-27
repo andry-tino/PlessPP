72a65f603e84e57cc640541f2955eaf6545b5359
Andrea <andry.tino@gmail.com>
2016-07-27 09:25:42 +0200

    Adding baseline provider

---
+++ src/PLessPP/AI/BaselineProvider.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// BaselineProvider.cs
+/// </summary>
+
+namespace PLessPP.AI
+{
+    using System;
+    using System.IO;
+
+    using PLessPP.Data;
+    using PLessPP.Data.Connectors;
+
+    /// <summary>
+    /// Provides the baseline.
+    /// </summary>
+    public class BaselineProvider
+    {
+        private const string sampleDataFileName = "SampleData_Andrea_Positive_001.csv";
+
+        // Cached values
+        private Sequence baseline;
+
+        /// <summary>
+        /// Gets the baseline.
+        /// </summary>
+        public Sequence Baseline
+        {
+            get
+            {
+                if (this.baseline == null)
+                {
+                    CSVDataConnector dataConnector = new CSVDataConnector(GetPath(sampleDataFileName));
+                    this.baseline = dataConnector.Data;
+                }
+                
+                return this.baseline;
+            }
+        }
+
+        private static string GetPath(string fileName)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "BaselineSequences", fileName);
+        }
+    }
+}
