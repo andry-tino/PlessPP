e7be5f675d4f4a4bed3c70c08367272571b6dea6
Andrea <andry.tino@gmail.com>
2016-07-30 00:57:07 +0200

    Normalizing AssemblyInfo files in all projects

--- src/Prod.MWMSSearchAlgorithm/Properties/AssemblyInfo.cs
+++ src/Prod.MWMSSearchAlgorithm/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PLessPP.MWMSSearchAlgorithm")]
+[assembly: AssemblyDescription("Multi Window Multi Shift Search algorithm")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AI")]
+[assembly: AssemblyCompany("PLessPP")]
+[assembly: AssemblyProduct("MWMSSearchAlgorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("EN")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Prod.PowerPointController/Properties/AssemblyInfo.cs
+++ src/Prod.PowerPointController/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PowerPointController")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PLessPP.PowerPointController")]
+[assembly: AssemblyDescription("Controller for driving a Power Point presentation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PLessPP")]
 [assembly: AssemblyProduct("PowerPointController")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("EN")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
